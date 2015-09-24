26f2add13ae5d68d7e446afb8c5419ad4708a95d
Ramon Boss <ramon.boss@swisscom.com>
2015-09-24 10:31:15 +0200

    added bump task and createReleaseBranch task

--- gulp/git-flow.js
+++ gulp/git-flow.js
@@ -1,18 +1,38 @@
 'use strict';
 
+var fs = require('fs');
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')({
   pattern: ['gulp-*']
 });
 
-gulp.task('git-flow', function() {
-  gulp.src('*')
+function bump(src, releaseType) {
+  return src
+    .pipe($.bump({type: releaseType}))
+    .pipe(gulp.dest('./'));
+}
+
+gulp.task('git-flow', ['bump', 'createReleaseBranch'], function() {
+  console.log('test');
+});
+
+gulp.task('bump', function () {
+  var src = gulp.src(['package.json', 'bower.json'])
     .pipe($.prompt.prompt({
       type: 'list',
       name: 'bump',
       message: 'What type of bump would you like to do?',
       choices: ['patch', 'minor', 'major']
-    }, function(res){
-      console.log(res);
+    }, function (res) {
+      src
+        .pipe($.bump({type: res.bump}))
+        .pipe(gulp.dest('./'));
     }));
+  return src;
+});
+
+gulp.task('createReleaseBranch', ['bump'], function() {
+  var json = JSON.parse(fs.readFileSync('package.json', 'utf8'));
+  var version = json.version;
+  $.git.checkout('release/' + version, {args:'-b'});
 });
